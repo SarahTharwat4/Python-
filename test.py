be6320d896c4dbe45019da344b312ce8a016f165
Sarah Tharwat <95343009+SarahTharwat4@users.noreply.github.com>
2024-02-14 16:50:51 +0200

    First project in python
    this project contain :
    how to print | comment | concatenation

---
+++ test.py
@@ -0,0 +1,13 @@
+# fisrt project in python => print Hello World 
+print ("Hello World")
+ ###########################################################################
+# comment
+# we use {#} for single line 
+# we use {''' comment '''} for multiple line 
+#############################################################################
+
+
+# concatenation => to combine strings 
+first="I Love "
+Second="Python"
+print (first +" "+Second )  # I Love Python => this is concatenation
